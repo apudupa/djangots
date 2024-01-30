9e74aca900652f308a3296a036c9936c54c0776f
Damian Gąsiorek <damian.gasiorek@techland.pl>
2024-01-30 14:03:34 +0100

    Revert "Update settings.py"
    This reverts commit 1667fa8243a5cc80a92d29f8e9c182c2785ff23f.

--- telemetry_store/settings.py
+++ telemetry_store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*...'
+SECRET_KEY = 'django-insecure-*-z&s#q#i@r4ts^ocq2h&-u(yr+8yp(p-=j=ubnaevs6!^z7tq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'telemetry',
         'USER': 'monkey',
-        'PASSWORD': 'pass',
+        'PASSWORD': 'rTRPmKui02TiYnpD',
         'HOST': 'db-master.tland.pl',
         'PORT': '3306',
         'OPTIONS': {
